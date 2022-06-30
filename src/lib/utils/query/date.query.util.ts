100b060c0c33167352f4cdfe7a6b145c776e2c76
rodericusifo <rodericus1999@gmail.com>
2022-06-30 19:02:09 +0700

    date query update

--- src/lib/utils/query/date.query.util.ts
+++ src/lib/utils/query/date.query.util.ts
@@ -2,19 +2,25 @@ import * as moment from 'moment';
 import { Between, LessThanOrEqual, MoreThanOrEqual } from 'typeorm';
 
 export const RangeDates = (from: Date | string, to: Date | string) => {
-  const dateFrom = moment(from);
-  const dateTo = moment(to);
-  dateFrom.subtract(1, 'days');
-  dateTo.add(1, 'days');
+  const dateFrom = moment(from, 'YYYY-MM-DD', true).isValid()
+    ? moment(from).startOf('day')
+    : moment(from);
+  const dateTo = moment(to, 'YYYY-MM-DD', true).isValid()
+    ? moment(to).endOf('day')
+    : moment(to);
   return Between(dateFrom.toISOString(), dateTo.toISOString());
 };
 
 export const FromDate = (value: Date | string) => {
-  const date = moment(value);
+  const date = moment(value, 'YYYY-MM-DD', true).isValid()
+    ? moment(value).startOf('day')
+    : moment(value);
   return MoreThanOrEqual(date.toISOString());
 };
 
 export const ToDate = (value: Date | string) => {
-  const date = moment(value);
+  const date = moment(value, 'YYYY-MM-DD', true).isValid()
+    ? moment(value).endOf('day')
+    : moment(value);
   return LessThanOrEqual(date.toISOString());
 };
